1a9ffc701f9e12f5a67b2cdfc42904933b399a85
Jing Jin <johnnyjinjing@gmail.com>
2018-01-18 17:32:25 -0800

    init commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './style/styles.scss';
+
+ReactDOM.render(<p>Hello world!</p>, document.getElementById('app'));
