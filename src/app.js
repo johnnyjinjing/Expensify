3ae246c25d25b59af1d872cbb35d5936ada79c7e
Jing Jin <johnnyjinjing@gmail.com>
2018-01-28 12:09:44 -0800

    setup test database env

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './style/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
 
